343e7d70ec41b8271020bc022fd621cb374d7b3a
MatthiasHelderald <109383974+MatthiasHelderald@users.noreply.github.com>
2022-10-24 11:11:44 +0200

    Init

--- Absorbtion Project/Assets/Scripts/SquareBehavior.cs
+++ Absorbtion Project/Assets/Scripts/SquareBehavior.cs
@@ -1,16 +1,20 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class SquareBehavior : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+    private void OnTriggerEnter(Collider other)
+    {
+        
+    }
+
     void Update()
     {
         
